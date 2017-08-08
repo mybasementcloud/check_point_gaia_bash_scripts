d56e2321675fe874c5550a5f04eb010cf764a766
Eric Beasley <mybasementcloud@gmail.com>
2017-08-08 01:00:38 -0500

    Changes 2017-08-08
    Updates and Files 2017-08-08-0100CDT

---
+++ config_capture/config_capture_002_v00.04.00.sh
@@ -0,0 +1,450 @@
+#!/bin/bash
+#
+# SCRIPT capture configuration values for bash and clish level 002
+#
+# (C) 2017 Eric James Beasley
+#
+ScriptVersion=00.04.00
+ScriptDate=2017-08-07
+#
+
+export BASHScriptVersion=v00x03x00
+
+#----------------------------------------------------------------------------------------
+# Setup Basic Parameters
+#----------------------------------------------------------------------------------------
+
+#points to where jq is installed
+#export JQ=${CPDIR}/jq/jq
+
+export DATE=`date +%Y-%m-%d-%H%M%S%Z`
+
+echo 'Date Time Group   :  '$DATE
+echo
+
+export outputpathroot=./host_data
+#export outputpathroot=/var/upgrade_export
+export outputpathbase=$outputpathroot
+export outputpathbase=$outputpathroot/$DATE
+
+export outputhomepath=$outputpathbase/home
+
+export homebackuproot=.
+export homebackuppath="$homebackuproot/home.backup"
+
+if [ ! -r $homebackuppath ] ; then
+    mkdir $homebackuppath
+    chmod 775 $homebackuppath
+else
+    chmod 775 $homebackuppath
+fi
+
+export gaiaversion=$(clish -c "show version product" | cut -d " " -f 6)
+echo 'Gaia Version : $gaiaversion = '$gaiaversion
+echo
+
+workfile=/var/tmp/cpinfo_ver.txt
+cpinfo -y all > $workfile 2>&1
+Check4EP773003=`grep -c "Endpoint Security Management R77.30.03 " $workfile`
+Check4EP773002=`grep -c "Endpoint Security Management R77.30.02 " $workfile`
+Check4EP773001=`grep -c "Endpoint Security Management R77.30.01 " $workfile`
+Check4EP773000=`grep -c "Endpoint Security Management R77.30 " $workfile`
+Check4EP=`grep -c "Endpoint Security Management" $workfile`
+Check4SMS=`grep -c "Security Management Server" $workfile`
+rm $workfile
+
+if [ "$MDSDIR" != '' ]; then
+    Check4MDS=1
+else 
+    Check4MDS=0
+fi
+
+if [ $Check4SMS -gt 0 ] && [ $Check4MDS -gt 0 ]; then
+    echo "System is Multi-Domain Management Server!"
+elif [ $Check4SMS -gt 0 ] && [ $Check4MDS -eq 0 ]; then
+    echo "System is Security Management Server!"
+else
+    echo "System is a gateway!"
+fi
+echo
+
+if [ $Check4EP773000 -gt 0 ] && [ $Check4EP773003 -gt 0 ]; then
+    echo "Endpoint Security Server version R77.30.03"
+    export gaiaversion=R77.30.03
+elif [ $Check4EP773000 -gt 0 ] && [ $Check4EP773002 -gt 0 ]; then
+    echo "Endpoint Security Server version R77.30.02"
+    export gaiaversion=R77.30.02
+elif [ $Check4EP773000 -gt 0 ] && [ $Check4EP773001 -gt 0 ]; then
+    echo "Endpoint Security Server version R77.30.01"
+    export gaiaversion=R77.30.01
+elif [ $Check4EP773000 -gt 0 ]; then
+    echo "Endpoint Security Server version R77.30"
+    export gaiaversion=R77.30
+else
+    echo "Not Gaia Endpoint Security Server"
+fi
+
+echo
+echo 'Final $gaiaversion = '$gaiaversion
+echo
+
+
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+#
+# shell meat
+#
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+
+
+#----------------------------------------------------------------------------------------
+# Configure specific parameters
+#----------------------------------------------------------------------------------------
+
+export targetversion=$gaiaversion
+
+export outputfilepath=$outputpathbase/
+export outputfileprefix=$HOSTNAME'_'$targetversion
+export outputfilesuffix='_'$DATE
+export outputfiletype=.txt
+
+if [ ! -r $outputpathroot ] ; then
+    mkdir $outputpathroot
+    chmod 775 $outputpathroot
+else
+    chmod 775 $outputpathroot
+fi
+if [ ! -r $outputpathbase ] ; then
+    mkdir $outputpathbase
+    chmod 775 $outputpathbase
+else
+    chmod 775 $outputpathbase
+fi
+if [ ! -r $outputfilepath ] ; then
+    mkdir $outputfilepath
+    chmod 775 $outputfilepath
+else
+    chmod 775 $outputfilepath
+fi
+if [ ! -r $outputhomepath ] ; then
+    mkdir $outputhomepath
+    chmod 775 $outputhomepath
+else
+    chmod 775 $outputhomepath
+fi
+
+#----------------------------------------------------------------------------------------
+# bash - backup user's home folder
+#----------------------------------------------------------------------------------------
+
+export command2run=backup-home
+export outputfile=$command2run'_'$outputfileprefix$outputfilesuffix
+export outputfilefqdn=$outputfilepath$outputfile
+
+echo
+echo 'Execute '$command2run' to '$outputhomepath' with ouptut to : '$outputfilefqdn
+touch "$outputfilefqdn"
+cp -a "$HOME" "$outputhomepath/" >> "$outputfilefqdn"
+cp -a "$HOME" "$homebackuppath/" >> "$outputfilefqdn" 
+
+
+
+#----------------------------------------------------------------------------------------
+# clish - save configuration to file
+#----------------------------------------------------------------------------------------
+
+export command2run=config
+export outputfile=$command2run'_'$outputfileprefix$outputfilesuffix
+export outputfilefqdn=$outputfilepath$outputfile
+
+echo
+echo 'Execute '$command2run' with ouptut to : '$outputfilefqdn
+clish -c "lock database override"
+clish -c "save config"
+clish -c "save configuration $outputfile"
+clish -c "save config"
+cp $outputfile $outputfilefqdn
+
+
+#----------------------------------------------------------------------------------------
+# clish and bash - Gather version information from all possible methods
+#----------------------------------------------------------------------------------------
+
+export command2run=versions
+export outputfile=$outputfileprefix'_'$command2run$outputfilesuffix$outputfiletype
+export outputfilefqdn=$outputfilepath$outputfile
+
+touch $outputfilefqdn
+echo 'Versions:' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+echo >> "$outputfilefqdn"
+echo 'clish : ' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+clish -c "show version all" >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+echo >> "$outputfilefqdn"
+echo '----------------------------------------------------------------------------' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+echo 'cpinfo -y all : ' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+cpinfo -y all >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+echo >> "$outputfilefqdn"
+echo '----------------------------------------------------------------------------' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+echo 'fwm ver : ' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+fwm ver >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+echo >> "$outputfilefqdn"
+echo '----------------------------------------------------------------------------' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+echo 'fw ver : ' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+fw ver >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+echo >> "$outputfilefqdn"
+echo '----------------------------------------------------------------------------' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+echo 'installed_jumbo_take : ' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+installed_jumbo_take >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+
+#----------------------------------------------------------------------------------------
+# bash - gather licensing information
+#----------------------------------------------------------------------------------------
+
+export command2run=cplic
+export outputfile=$outputfileprefix'_'$command2run$outputfilesuffix$outputfiletype
+export outputfilefqdn=$outputfilepath$outputfile
+
+echo
+echo 'Execute '$command2run' with ouptut to : '$outputfilefqdn
+cplic print > "$outputfilefqdn"
+
+
+#----------------------------------------------------------------------------------------
+# bash - gather route details
+#----------------------------------------------------------------------------------------
+
+export command2run=route
+export outputfile=$outputfileprefix'_'$command2run$outputfilesuffix$outputfiletype
+export outputfilefqdn=$outputfilepath$outputfile
+
+echo
+echo 'Execute '$command2run' with ouptut to : '$outputfilefqdn
+route -vn > "$outputfilefqdn"
+
+
+#----------------------------------------------------------------------------------------
+# bash - gather arp details
+#----------------------------------------------------------------------------------------
+
+export command2run=arp
+export outputfile=$outputfileprefix'_'$command2run$outputfilesuffix$outputfiletype
+export outputfilefqdn=$outputfilepath$outputfile
+
+echo
+echo 'Execute '$command2run' with ouptut to : '$outputfilefqdn
+
+touch $outputfilefqdn
+arp -vn >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+echo '----------------------------------------------------------------------------' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+arp -av >> "$outputfilefqdn"
+
+
+#----------------------------------------------------------------------------------------
+# bash - gather interface details
+#----------------------------------------------------------------------------------------
+
+export command2run=ifconfig
+export outputfile=$outputfileprefix'_'$command2run$outputfilesuffix$outputfiletype
+export outputfilefqdn=$outputfilepath$outputfile
+
+echo
+echo 'Execute '$command2run' with ouptut to : '$outputfilefqdn
+ifconfig > "$outputfilefqdn"
+
+
+#----------------------------------------------------------------------------------------
+# bash - gather interface name rules
+#----------------------------------------------------------------------------------------
+
+export command2run=interface-naming-rules
+export outputfile=$outputfileprefix'_'$command2run$outputfilesuffix$outputfiletype
+export outputfilefqdn=$outputfilepath$outputfile
+export file2copy=00-OS-XX.rules
+export file2copypath="/etc/udev/rules.d/$file2copy"
+
+echo
+echo 'Execute '$command2run' with ouptut to : '$outputfilefqdn
+cat "$file2copypath" > "$outputfilefqdn"
+cp "$file2copypath" "$outputfilepath"
+
+
+#----------------------------------------------------------------------------------------
+# bash - generate device and system information via dmidecode
+#----------------------------------------------------------------------------------------
+
+export command2run=dmidecode
+export outputfile=$outputfileprefix'_'$command2run$outputfilesuffix$outputfiletype
+export outputfilefqdn=$outputfilepath$outputfile
+
+echo
+echo 'Execute '$command2run' with ouptut to : '$outputfilefqdn
+dmidecode > "$outputfilefqdn"
+
+
+#----------------------------------------------------------------------------------------
+# bash - collect $FWDIR/boot/modules/fwkern.conf and backup if it exists
+#----------------------------------------------------------------------------------------
+
+# $FWDIR/boot/modules/fwkern.conf
+export file2copy=fwkern.conf
+export file2copypath="$FWDIR/boot/modules/$file2copy"
+export outputfile=$outputfileprefix'_'$file2copy$outputfilesuffix
+export outputfilefqdn=$outputfilepath$outputfile
+
+if [ ! -r $file2copypath ] ; then
+    echo
+    echo 'No fwkern.conf file at :  '$file2copypath
+else
+    echo
+    echo 'copy '$file2copy' to : '"$outputfilefqdn"
+    cp "$file2copypath" "$outputfilefqdn"
+    cp "$file2copypath" "$outputfilepath"
+    cp "$file2copypath" .
+fi
+echo
+
+
+#----------------------------------------------------------------------------------------
+# bash - status of SecureXL
+#----------------------------------------------------------------------------------------
+
+export command2run=fwaccel-statistics
+export outputfile=$outputfileprefix'_'$command2run$outputfilesuffix$outputfiletype
+export outputfilefqdn=$outputfilepath$outputfile
+
+echo
+echo 'Execute '$command2run' with ouptut to : '$outputfilefqdn
+
+touch $outputfilefqdn
+echo >> "$outputfilefqdn"
+echo 'fwacell stat' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+fwaccel stat >> "$outputfilefqdn"
+
+echo >> "$outputfilefqdn"
+echo '----------------------------------------------------------------------------' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+echo 'fwacell stats' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+fwaccel stats >> "$outputfilefqdn"
+
+echo >> "$outputfilefqdn"
+echo '----------------------------------------------------------------------------' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+echo 'fwacell stats -s' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+fwaccel stats -s >> "$outputfilefqdn"
+
+
+#----------------------------------------------------------------------------------------
+# bash - basic information
+#----------------------------------------------------------------------------------------
+
+export command2run=basic_information
+export outputfile=$outputfileprefix'_'$command2run$outputfilesuffix$outputfiletype
+export outputfilefqdn=$outputfilepath$outputfile
+
+echo
+echo 'Execute '$command2run' with ouptut to : '$outputfilefqdn
+
+touch $outputfilefqdn
+echo >> "$outputfilefqdn"
+echo 'Memory Utilization : free -m -t' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+free -m -t >> "$outputfilefqdn"
+
+echo >> "$outputfilefqdn"
+echo '----------------------------------------------------------------------------' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+echo 'Disk Utilization : df -h' >> "$outputfilefqdn"
+echo >> "$outputfilefqdn"
+
+df -h >> "$outputfilefqdn"
+
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+#
+
+#----------------------------------------------------------------------------------------
+# bash - ?what next?
+#----------------------------------------------------------------------------------------
+
+#export command2run=command
+#export outputfile=$outputfileprefix'_'$command2run$outputfilesuffix$outputfiletype
+#export outputfilefqdn=$outputfilepath$outputfile
+
+#echo
+#echo 'Execute '$command2run' with ouptut to : '$outputfilefqdn
+#command > "$outputfilefqdn"
+
+#echo '----------------------------------------------------------------------------' >> "$outputfilefqdn"
+#echo >> "$outputfilefqdn"
+#echo 'fwacell stats -s' >> "$outputfilefqdn"
+#echo >> "$outputfilefqdn"
+#
+#fwaccel stats -s >> "$outputfilefqdn"
+#
+
+
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+#
+# end shell meat
+#
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+
+
+
+echo 'CLI Operations Completed'
+
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+#
+# shell clean-up and log dump
+#
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+
+echo
+ls -alh $outputpathroot
+#ls -alhR $outputpathroot
+echo
+#echo
+#ls -alhR $outputpathbase
+#echo
+
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+# End of Script
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+
